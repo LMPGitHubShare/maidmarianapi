d131da790575d53ff62a1cdae7793aea2603ffe1
LMPGitHubShare <78520220+LMPGitHubShare@users.noreply.github.com>
2022-03-02 16:24:02 -0500

    Create maidmarian.py
    added api

---
+++ maidmarian.py
@@ -0,0 +1,17 @@
+import urllib.request
+import json
+
+enpoints = "https://maidmarian.azurewebsites.net"
+
+def createaccount(name, password, apikey):
+    return urllib.request.urlopen(enpoints + "/Api/CreateAccount?accountId=" + name +"&password="+ password + "&apiKey=" + apikey, data={}).read()
+
+def getaccount(name, password, apikey):
+    return json.loads(urllib.request.urlopen(enpoints + "/Api/GetAccount?accountId=" + name +"&password="+ password + "&apiKey=" + apikey, data={}).read())
+
+def buy(name, ticker, fraction, password, apikey):
+    return json.loads(urllib.request.urlopen(enpoints + "/Api/Buy?accountId=" + name + "&symbole=" + ticker + "&fraction=" + fraction +"&password="+ password + "&apiKey=" + apikey, data={}).read())
+
+def sell(name, stockid, password, apikey):
+    return json.loads(urllib.request.urlopen(enpoints + "/Api/Sell?accountId=" + name + "&stockId=" + stockid  +"&password="+ password + "&apiKey=" + apikey, data={}).read())
+    

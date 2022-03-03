13090b98d12a48e88cbc37f8b38a0d3ec38e4785
Masterlau Lau <Masterlau2@hotmail.com>
2022-03-02 20:27:44 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='maidmarianapi',
+   version='0.0.1',
+   description='Maid Marian API',
+   author='LMP',
+   author_email='masterlau4@hotmail.com',
+   packages=['maidmarianapi'],  #same as name
+   install_requires=['wheel'], #external packages as dependencies
+)
